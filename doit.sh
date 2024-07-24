c71e460c86731689197e9a0e958d9239c74628dc
Emile Aben <emileaben@gmail.com>
2024-07-24 06:43:39 +0000

    last commit before push

--- doit.sh
+++ doit.sh
@@ -1,12 +1,13 @@
 YYYY=2024
 MM=07
-DD=01
+DD=23
 
 # minimum number of RIS peers that see a prefix (before the prefix is considered 'significant'
 THRES=28 
 
 mkdir -p ./data
 
+date
 echo "creating peer.pfx.pathlen.$YYYY.$MM.$DD.txt.gz"
 if [ ! -f peer.pfx.pathlen.$YYYY.$MM.$DD.txt.gz ]; then
    for RRC in /mnt/ris/rrc??/$YYYY.$MM/bview.$YYYY$MM$DD.0000.gz
@@ -17,6 +18,7 @@ fi
 
 ### creates a file with
 # peers/prefixes (count) (min dist) (max dist)
+date
 echo "creating stats.$YYYY.$MM.$DD.txt.gz"
 if [ ! -f stats.$YYYY.$MM.$DD.txt.gz ]; then
    ./dist-stats.py ./data/peer.pfx.pathlen.$YYYY.$MM.$DD.txt.gz $YYYY-$MM-$DD | gzip -9 > ./data/stats.$YYYY.$MM.$DD.txt.gz
@@ -24,11 +26,12 @@ fi
 
 ### improvements files has
 ## address family ASN improvement factor (sum AS-hops per peer)
+date
 echo "creating improvements.$YYYY.$MM.$DD.txt"
 if [ ! -f improvements.$YYYY.$MM.$DD.txt.gz ]; then
    for RRC in /mnt/ris/rrc??/$YYYY.$MM/bview.$YYYY$MM$DD.0000.gz
    do
        bgpdump -m -t change $RRC
    #done | ./calc-dist-improvements.py peer.pfx.pathlen.$YYYY.$MM.$DD.txt.gz stats.$YYYY.$MM.$DD.txt.gz $THRES | gzip -9 > improvements.$YYYY.$MM.$DD.txt.gz
-   done | ./calc-dist-improvements.py ./data/peer.pfx.pathlen.$YYYY.$MM.$DD.txt.gz ./data/stats.$YYYY.$MM.$DD.txt.gz $THRES $YYYY-$MM-$DD | tee ./data/improvements.$YYYY.$MM.$DD.txt
+   done | ./calc-dist-improvements.py ./data/peer.pfx.pathlen.$YYYY.$MM.$DD.txt.gz ./data/stats.$YYYY.$MM.$DD.txt.gz $THRES $YYYY-$MM-$DD > ./data/improvements.$YYYY.$MM.$DD.txt
 fi
